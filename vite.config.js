679f426448db7fac04c02d8f93be5b32e4934976
Brandon Pouliot <brandonpouliot@gmail.com>
2024-05-23 20:49:46 -0400

    added new folders and file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+test: {
+  globals: true,
+  environment: 'happy-dom',
+  setupFiles: './src/tests/setup.js',
+},
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
